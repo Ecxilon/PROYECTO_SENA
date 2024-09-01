a3386769e1baa6d0a2600af9eff78311483053aa
CH4 <mitigatedsoup57@gmail.com>
2024-09-01 15:44:43 -0500

    change the files

--- Model/MenuDesplegable.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('toggleDropdown').addEventListener('click', function() {
-    var dropdown = document.getElementById('dropdownMenu');
-    if (dropdown.style.display === 'none' || dropdown.style.display === '') {
-      dropdown.style.display = 'block';
-    } else {
-      dropdown.style.display = 'none';  
-    }
-  });
